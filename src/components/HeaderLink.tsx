5d3fd3387bea1751add67ba22e34e014671c61e3
oncet <camilorivera86@gmail.com>
2021-08-12 00:35:08 -0300

    add header link component

---
+++ src/components/HeaderLink.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Link } from "@chakra-ui/react";
+
+type HeaderLinkProps = {
+  to: string;
+  onClick: () => void;
+};
+
+export const HeaderLink: React.FC<HeaderLinkProps> = ({
+  to,
+  onClick,
+  children,
+}) => {
+  return (
+    <Link
+      as={RouterLink}
+      onClick={onClick}
+      alignItems="center"
+      display="flex"
+      px={1}
+      py={2}
+      to={to}
+    >
+      {children}
+    </Link>
+  );
+};
