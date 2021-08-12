d75c725057845a28527ae523ed0ea44e23bfe282
oncet <camilorivera86@gmail.com>
2021-08-11 23:51:30 -0300

    apply default stat style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from "react";
-import { chakra, ChakraProvider, Container, theme } from "@chakra-ui/react";
+import {
+  chakra,
+  ChakraProvider,
+  Container,
+  extendTheme,
+  // theme,
+} from "@chakra-ui/react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
 import { Contracts } from "./pages/Contracts";
 import { Footer } from "./components/Footer";
+
+const theme = extendTheme({
+  components: {
+    Stat: {
+      baseStyle: {
+        container: {
+          background: "purple.900",
+          border: "1px solid",
+          borderColor: "purple.600",
+          borderRadius: "md",
+          px: "4",
+          py: "2",
+        },
+      },
+    },
+  },
+});
 
 export const App = () => (
   <ChakraProvider theme={theme}>
